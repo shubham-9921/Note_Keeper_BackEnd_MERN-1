0c2e172397b38bd8b599f084bd0fe17ac318363b
Shubham Bodkhe <bodkheshubham9921@gmail.com>
2024-10-04 00:35:44 +0530

    User Reistation Done

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, pic } = req.body;
+
+    const existUser = await User.findOne({ email });
+
+    if (existUser) {
+        res.status(400);
+        throw new Error("User Already Exists!");
+    }
+
+    const user = await User.create({
+        name, email, password, pic
+    })
+
+    if (user) {
+        res.status(201)
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(400);
+        throw new Error("Error in Registration");
+    }
+})
+
+module.exports = { registerUser }
--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
-
-
+const cors = require('cors')
 const app = express();
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
+
+
+app.use(express.json());
+app.use(cors());
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("App is running");
 
 });
 
 // Get all notes 
-app.get("/api/notes",(req,res)=>{
+app.get("/api/notes", (req, res) => {
     res.json(notes)
 });
 
-app.get("/api/notes/:id",(req,res)=>{
-    const currentNote = notes.filter((notes)=>(notes._id === req.params.id));
+app.use('/api/users', userRoutes)
+app.use(notFound);
+app.use(errorHandler);
 
-    res.json(currentNote);
-});
-
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
